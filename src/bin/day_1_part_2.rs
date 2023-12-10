ad2fe71d424bf2d37f12fa9212090268882927d7
Santiago Díaz Guevara <santiagodg@uber.com>
2023-12-09 20:19:20 -0800

    Work on day 1 part 2 some more

--- src/bin/day_1_part_2.rs
+++ src/bin/day_1_part_2.rs
@@ -1,17 +1,15 @@
 use std::{path::Path, io::{self, BufRead}, fs::File};
-use nom::{branch::alt, bytes::complete::tag, multi::length_count};
-use nom::character::complete::char;
 
 fn main() {
     if let Ok(lines) = read_lines("./input/day_1.txt") {
-        let mut sum = 0;
+        let mut sum: i64 = 0;
 
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
             if let Ok(ip) = line {
                 println!("line: {ip}");
 
-                let digitted_line = spelled_to_digits(ip);
+                let digitted_line = spelled_to_digits(ip.clone());
                 println!("digitted line: {digitted_line}");
 
                 let v = calibration_value(digitted_line);
@@ -31,7 +29,7 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
-fn calibration_value(line: String) -> i32 {
+fn calibration_value(line: String) -> i64 {
     let mut first_digit: Option<char> = None;
     let mut last_digit: Option<char> = None;
 
@@ -49,7 +47,7 @@ fn calibration_value(line: String) -> i32 {
     }
 
     if first_digit.is_some() && last_digit.is_none() {
-        return format!("{}", first_digit.unwrap()).parse().unwrap()
+        return format!("{}{}", first_digit.unwrap(), first_digit.unwrap()).parse().unwrap()
     }
 
     format!("{}{}", first_digit.unwrap(), last_digit.unwrap()).parse().unwrap()
@@ -60,35 +58,28 @@ fn spelled_to_digits(mut line: String) -> String {
 
     while line.len() != 0 {
         if line.starts_with("one") {
-            line.drain(..3);
             result.push_str("1");
         } else if line.starts_with("two") {
-            line.drain(..3);
             result.push_str("2");
         } else if line.starts_with("three") {
-            line.drain(..5);
             result.push_str("3");
         } else if line.starts_with("four") {
-            line.drain(..4);
             result.push_str("4");
         } else if line.starts_with("five") {
-            line.drain(..4);
             result.push_str("5");
         } else if line.starts_with("six") {
-            line.drain(..3);
             result.push_str("6");
         } else if line.starts_with("seven") {
-            line.drain(..5);
             result.push_str("7");
         } else if line.starts_with("eight") {
-            line.drain(..5);
             result.push_str("8");
         } else if line.starts_with("nine") {
-            line.drain(..4);
             result.push_str("9");
         } else {
-            result.push_str(&line.drain(..1).collect::<String>())
+            result.push(line.chars().nth(0).unwrap())
         }
+
+        line.drain(..1);
     }
 
     result
